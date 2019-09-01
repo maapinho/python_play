cea39d5f818eae973256efc674de7c23c0abe08b
Mario P <maapinho@hotmail.com>
2019-09-01 22:08:42 +0200

    Update mathgame

--- mathgame.py
+++ mathgame.py
@@ -1,27 +1,52 @@
+# To do
+# pick the range of numbers
+# Loop
+# error catching for inputs (must be an integer)
+# answers list from external files
+
 
 import random
 
+# Constants
+
 DEFAULTRANGE = 10
-WRONGANSWER = "wrong answer!"
-CORRECTANSWER= "correct answer!"
+WRONGANSWERLIST = ["wrong answer!",
+                   "Dumbass!",
+                   "WTF??? How old are you?"]
+CORRECTANSWERLIST= ["Correct Answer!",
+                    "Niiiice!",
+                    "Einstein!"]
 OPERATIONS = ["product","addition", "subtraction"]
-INPUTQUESTION="What is the result?"
+INPUTQUESTION="What is the result of "
  
 # Pick the highest number. Leave empty for default (10)
 
-RANGE=DEFAULTRANGE
+# function to pick a random item from a list
 
-value1 = random.randint(0,RANGE)
-value2 = random.randint(0,RANGE)
+def randomitem(inputlist):
+    return inputlist[random.randint(0,len(inputlist)-1)]
 
-print("values",value1,value2)
+# Range of values for expression's input
 
-# Pick a random operation
-op_random = random.randint(0,len(OPERATIONS)-1)
-print("op_random",op_random)
+valuerange=input("Pick a range of values from 0 to 10 (default is 10): ")
 
-operation = OPERATIONS[op_random]
-print(operation)
+if valuerange=="":
+    valuerange=DEFAULTRANGE
+
+###### Pending error catching keeping within the value range of 0 to 10
+
+valuerange=int(valuerange)
+
+
+#valuerange=DEFAULTRANGE
+
+value1 = random.randint(0,valuerange)
+value2 = random.randint(0,valuerange)
+
+
+
+operation = randomitem(OPERATIONS)
+#print(operation)
 
 # Get the result from that random operation
 
@@ -32,20 +57,31 @@ OPERATIONSMAP = {"product": {"character":"x","expression":value1*value2},
 
 result = OPERATIONSMAP[operation]["expression"]
 
-print ("What is the result of",value1,operation,value2)
+# Ask for the result from the user
+
+
+print ("What is the result of",value1,OPERATIONSMAP[operation]["character"],value2)
 
 inputresult=int(input(INPUTQUESTION))
 
-print("your answer",inputresult)
-print("result",result)
+#print("your answer",inputresult)
+#print("result",result)
 
-if inputresult==result:
-    print(CORRECTANSWER)
-else:
-    print(WRONGANSWER)
-
-
-# Ask for the result from the user
+# Pick random answers from the answer lists
+correct_answer=randomitem(CORRECTANSWERLIST)
+wrong_answer=randomitem(WRONGANSWERLIST)
 
 # Compare both results and present the answer.
+
+#----------- old method with a single answer
+#if inputresult==result:
+#    print(CORRECTANSWER)
+#else:
+#    print(WRONGANSWER)
+#----------------------------------------
+
+if inputresult==result:
+    print(correct_answer)
+else:
+    print(wrong_answer)
 
