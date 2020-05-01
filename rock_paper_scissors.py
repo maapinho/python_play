2480142e4bfae00acdd089264cc651d619552797
Mario <maapinho@hotmail.com>
2020-05-02 01:14:03 +0200

    Added winner logic

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -8,6 +8,11 @@ from time import sleep
 # Constants
 BARS=20
 OPTIONS=['Rock','Paper','Scissors']
+WINNERLEFT = [
+    ['Rock','Scissors'],
+    ['Paper','Rock'],
+    ['Scissors','Paper']
+]
 
 def bars():
     print('-'*BARS)
@@ -16,6 +21,11 @@ def bars():
 # Function for the computer pick
 def computerpick():
     return OPTIONS[randint(0,len(OPTIONS)-1)]
+
+# Decide on winner
+# Return True if first element is winner
+def winner(choice1,choice2):
+    return [choice1,choice2] in WINNERLEFT
 
 # Print options and ask for player option
 for i in OPTIONS:
@@ -35,7 +45,16 @@ computer=computerpick()
 bars()
 print('Computer pick: ',computer)
 bars()
+print('\n')
 
 # Compare picks and declare a winner
+
+if computer == player:
+    print("Tie. Play again")
+elif winner(player,computer):
+    print('You won!\n')
+else:
+    print('You lost\n')
+
 
 # hmmmm...
