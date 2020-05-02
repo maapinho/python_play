24ad12af71a4d54bea8e38c257883114d104fe97
Mario <maapinho@hotmail.com>
2020-05-02 14:24:23 +0200

    While loop for the main program

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -32,42 +32,44 @@ def computerpick():
 def winner(choice1,choice2):
     return [choice1,choice2] in WINNERLEFT
 
-# Print options and ask for player option
-for i in OPTIONS:
-    print(OPTIONS.index(i)+1,':',i)
-
 while True:
-    try:
-        optstr=input('Pick your option (1-3):')
-        opt=int(optstr)
-        player=OPTIONS[opt-1]
-        break
-    except ValueError:
-        print('String input. The value must be a number between 1 and 3 \n')
-    except IndexError:
-        print('Wrong value. The number must be between 1 and 3 \n')
 
-bars()
-print('Player pick: ',player)
-bars()
-sleep(2)
+    # Print options and ask for player option
+    for i in OPTIONS:
+        print(OPTIONS.index(i)+1,':',i)
+
+    while True:
+        try:
+            optstr=input('Pick your option (1-3):')
+            opt=int(optstr)
+            player=OPTIONS[opt-1]
+            break
+        except ValueError:
+            print('String input. The value must be a number between 1 and 3 \n')
+        except IndexError:
+            print('Wrong value. The number must be between 1 and 3 \n')
+
+    bars()
+    print('Your pick: ',player)
+    bars()
+    sleep(2)
 
 
-# Computer pick
-computer=computerpick()
-bars()
-print('Computer pick: ',computer)
-bars()
-print('\n')
+    # Computer pick
+    computer=computerpick()
+    bars()
+    print('Computer pick: ',computer)
+    bars()
+    print('\n')
 
-# Compare picks and declare a winner
+    # Compare picks and declare a winner
 
-if computer == player:
-    print(RESULTTIE,'\n')
-elif winner(player,computer):
-    print(RESULTWIN,'\n')
-else:
-    print(RESULTLOSE,'\n')
+    if computer == player:
+        print(RESULTTIE,'\n')
+    elif winner(player,computer):
+        print(RESULTWIN,'\n')
+    else:
+        print(RESULTLOSE,'\n')
 
 
 # hmmmm...
